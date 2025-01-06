5c2d6f63cbd4d0626d397e8d4fe0c52276e1941d
NasNeo <1270071138@qq.com>
2025-01-06 09:26:15 +0800

    mps

--- Config.py
+++ Config.py
@@ -1,3 +1,10 @@
 import torch
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+if torch.backends.mps.is_available():
+    device = torch.device("mps")  # 使用 MPS
+elif torch.cuda.is_available():
+    device = torch.device("cuda:0")  # 使用 CUDA
+else:
+    device = torch.device("cpu")  # 使用 CPU
+
+print(f"Using device: {device}")
